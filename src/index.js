ee5b7f07272182ba2dc1035b036d25c4c710d4c0
Joel Luis Steffen <jo.lsteffen@gmail.com>
2023-11-11 17:27:08 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
-import express from 'express';
+/*
+Aplicação Lista de Recados
+● Vamos criar um back-end que contém
+as seguintes funcionalidades:
+○ Criação de conta
+○ Login
+○ CRUD* de recados
+
+● Dados de um usuário:
+○ Identificador
+○ Nome
+○ E-mail
+○ Senha
+
+● Dados de um recado:
+○ Identificador
+○ Título
+○ Descrição
+
+Regras
+● Não pode ter mais de um usuário com o mesmo e-mail.
+● O login deve ser feito com e-mail e senha.
+● Cada recado deve ser de um único usuário.
+*/
+
+import express from "express";
 const app = express();
 app.use(express.json());
 
-app.get('/', (request, response) => {
-return response.json('OK');
+const usuarios = [
+    {
+      identificador: 0,
+      nome: "Exemplo",
+      email: "exemplo@exemplo.com",
+      senha: "12345",
+    },
+  ];
+  const recados = [{ identificador: 0, titulo: "Exemplo", descricao: "exemplo" }];
 
+app.get("/usuario", (request, response) => {
+  return response.json("OK");
 });
+
 
 app.listen(8080, () => console.log("Servidor iniciado"));
